013ef46b180b53ac847f471cd9f2a688d3423aa5
EnricoLGM <enricolegumi44@gmail.com>
2022-02-22 16:44:30 +0100

    Aggiunta lettura file raw

--- app/src/main/java/com/example/filemanager/GestioneFIle.java
+++ app/src/main/java/com/example/filemanager/GestioneFIle.java
@@ -1,13 +1,17 @@
 package com.example.filemanager;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.util.Log;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class GestioneFIle {
     String nomeFile;
@@ -41,6 +45,7 @@ public class GestioneFIle {
         return sb.toString();
     }
 
+    //stream orientato ai byte
     public String writeFile(String nF, Context c, String cont) {
         FileOutputStream fileOut;
         String risultato;
@@ -61,5 +66,44 @@ public class GestioneFIle {
             e.printStackTrace();
         }
         return risultato;
+    }
+
+    //che utilizza il buffered writer che legge una riga di caratteri
+    public String writeFileBuffered(String nF, Context c, String cont) {
+        FileOutputStream fileOut;
+        String risultato;
+        BufferedWriter writer;
+        try {
+            writer=new BufferedWriter(new OutputStreamWriter(c.openFileOutput(nF, Context.MODE_PRIVATE)));
+            risultato="File Scritto Correttamente";
+        }
+        catch (FileNotFoundException e) {
+            risultato="Errore";
+            Log.e(TAG,"File Non Trovato");
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            risultato="Errore";
+            Log.e(TAG,"Errore I/O");
+            e.printStackTrace();
+        }
+        return risultato;
+    }
+
+    public String leggiRawFile(Context c) {
+        String testo;
+        StringBuilder sb=new StringBuilder();
+        Resources res=c.getResources();
+        InputStream is=res.openRawResource(R.raw.brani);
+        BufferedReader br=new BufferedReader(new InputStreamReader(is));
+        try {
+            while((testo=br.readLine())!=null) {
+                sb.append(testo + "\n");
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "I/O Exception");
+            e.printStackTrace();
+        }
+        return sb.toString();
     }
 }
