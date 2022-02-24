3d7a893783ef6b1e9f311c68a2f7aba63a4491da
EnricoLGM <enricolegumi44@gmail.com>
2022-02-24 16:39:20 +0100

    Aggiunti bottono per vari metodi di lettura di un file

--- app/src/main/java/com/example/filemanager/MainActivity.java
+++ app/src/main/java/com/example/filemanager/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     EditText nomeFile;
     Button lettura;
     Button scrittura;
+    Button assets;
+    Button raw;
 
     GestioneFIle gestore;
 
@@ -26,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
         nomeFile=(EditText) findViewById(R.id.nomeFile);
         lettura=(Button) findViewById(R.id.lettura);
         scrittura=(Button) findViewById(R.id.scrittura);
+
+        raw=(Button) findViewById(R.id.raw);
+        assets=(Button) findViewById(R.id.assets);
 
         gestore = new GestioneFIle();
 
@@ -42,6 +47,22 @@ public class MainActivity extends AppCompatActivity {
                 //nomeFile inserito come prova per l'EditText
                 String risultato=gestore.writeFile("fileDaLeggere.txt", getApplicationContext(), nomeFile.getText().toString());
                 Toast.makeText(getApplicationContext(), risultato, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        assets.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String risultato=gestore.leggiAssetsFile(getApplicationContext());
+                contenutoFile.setText(risultato);
+            }
+        });
+
+        raw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String risultato=gestore.leggiRawFile(getApplicationContext());
+                contenutoFile.setText(risultato);
             }
         });
     }
